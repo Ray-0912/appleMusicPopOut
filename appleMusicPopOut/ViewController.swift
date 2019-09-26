d09d5359adfe3ff87792d391ee6cec8dff11eda0
Limwris <limwris0912@gmail.com>
2019-09-27 00:19:01 +0800

    Initial Commit

---
+++ appleMusicPopOut/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  appleMusicPopOut
+//
+//  Created by ShengJung on 2019/9/27.
+//  Copyright © 2019 ShengJung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ appleMusicPopOutTests/appleMusicPopOutTests.swift
@@ -0,0 +1,34 @@
+//
+//  appleMusicPopOutTests.swift
+//  appleMusicPopOutTests
+//
+//  Created by ShengJung on 2019/9/27.
+//  Copyright © 2019 ShengJung. All rights reserved.
+//
+
+import XCTest
+@testable import appleMusicPopOut
+
+class appleMusicPopOutTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
